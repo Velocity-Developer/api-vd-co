e2615601de193817be345132e51b967d0471f3cd
adityathok <aditya0thok@gmail.com>
2026-06-15 11:19:33 +0700

    feat: api/v1/license

---
+++ app/Http/Controllers/Api/V1/LicenseController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\License;
+use App\Models\Website;
+
+class LicenseController extends Controller
+{
+    public function index(Request $request, License $license)
+    {
+        // Mengambil nilai header 'license'
+        $license = $request->header('license');
+
+        // Cek jika license tidak valid
+        $license = License::where('code', $license)->first();
+
+        // Jika license tidak valid
+        if (!$license) {
+            return response()->json([
+                'status' => false,
+                'message' => 'License not found',
+            ], 404);
+        }
+
+        // Mengambil nilai header 'source'
+        $source = $request->header('source');
+        $website = $source ? Website::where('domain', $source)->first() : null;
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Success',
+            'data' => [
+                'status' => $license->toArray()['is_active'],
+                'is_active' => $license->toArray()['is_active'],
+                'code' => $license->toArray()['code'],
+                'website' => $website ? $website->toArray()['domain'] : null,
+            ],
+        ]);
+    }
+}
