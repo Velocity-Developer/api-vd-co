fc4e050cccd9bea754f34edb5bca32892a783d8a
adityathok <aditya0thok@gmail.com>
2026-06-20 14:04:56 +0700

    feat: public/v1 use public.ai.signature

--- routes/api_public.php
+++ routes/api_public.php
@@ -3,10 +3,6 @@
 use App\Http\Controllers\ApiPublic\V1\ProjectPublicController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('public/v1')->group(function () {
+Route::middleware('public.ai.signature')->prefix('public/v1')->group(function () {
     Route::get('/project/{slug}', [ProjectPublicController::class, 'show']);
-});
-
-Route::middleware('public.ai.signature')->prefix('ai/public')->group(function () {
-    //
 });
